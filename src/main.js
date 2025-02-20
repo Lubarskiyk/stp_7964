c142fe44247ba18424de4c1db16258c8ded5cc89
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2025-02-20 14:52:59 +0200

    add CookieBaner

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { initBurgerMenu } from './js/burgerMenu.js';
+import { cookieBaner } from './js/cookieBaner.js';
 import { initHeaderScroll } from './js/headerScroll.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initBurgerMenu();
 });
 initHeaderScroll();
+cookieBaner();
