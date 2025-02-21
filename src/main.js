aa2aa3c3fee6a4ac0b0a8220e835d834f2fb429f
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2025-02-22 00:19:48 +0100

    add gallery swiper, fix anchors, fix header scroll

--- src/main.js
+++ src/main.js
@@ -3,19 +3,27 @@ import { initBurgerMenu } from './js/burgerMenu.js';
 import { cookieBaner } from './js/cookieBaner.js';
 import { faqAcordion } from './js/faq.js';
 import { initHeaderScroll } from './js/headerScroll.js';
+import { initSectionScroll } from './js/sectionScroll.js';
 import { observeSwiper } from './js/swiper.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initBurgerMenu();
 });
 initHeaderScroll();
+initSectionScroll();
 cookieBaner();
 toggleText();
 faqAcordion();
 
 observeSwiper('.benefits', {
-  containerSelector: '.swiper',
+  containerSelector: '.benefits__slider',
   paginationSelector: '.benefits__pagination',
   nextButtonSelector: '.benefits__button-next',
   prevButtonSelector: '.benefits__button-prev',
+});
+observeSwiper('.gallery', {
+  containerSelector: '.gallery__slider',
+  paginationSelector: '.gallery__pagination',
+  nextButtonSelector: '.gallery__button-next',
+  prevButtonSelector: '.gallery__button-prev',
 });
