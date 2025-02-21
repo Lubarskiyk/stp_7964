4e24ff855af0a3f43be95e0fd6603b477aef6cf7
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2025-02-21 16:11:53 +0100

    add swiper desktop

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { toggleText } from './js/aboutOpenCloseText.js';
 import { initBurgerMenu } from './js/burgerMenu.js';
 import { cookieBaner } from './js/cookieBaner.js';
 import { initHeaderScroll } from './js/headerScroll.js';
+import './js/swiper.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initBurgerMenu();
@@ -9,4 +10,3 @@ document.addEventListener('DOMContentLoaded', () => {
 initHeaderScroll();
 cookieBaner();
 toggleText();
-
