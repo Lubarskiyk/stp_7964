cf222d0c7d9418ecf815e20872fe2bcde9b949cc
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2025-02-23 14:57:36 +0100

    add swiper flip

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { cookieBaner } from './js/cookieBaner.js';
 import { faqAcordion } from './js/faq.js';
 import { initHeaderScroll } from './js/headerScroll.js';
 import { initSectionScroll } from './js/sectionScroll.js';
+import { observeBenefitsSwiper } from './js/benefitsSwiper.js';
 import { observeSwiper } from './js/swiper.js';
 import { observeReviewsSwiper } from './js/swiperReviews.js';
 
@@ -16,18 +17,20 @@ cookieBaner();
 toggleText();
 faqAcordion();
 
-observeSwiper('.benefits', {
+observeBenefitsSwiper('.benefits', {
   containerSelector: '.benefits__slider',
   paginationSelector: '.benefits__pagination',
   nextButtonSelector: '.benefits__button-next',
   prevButtonSelector: '.benefits__button-prev',
 });
+
 observeSwiper('.gallery', {
   containerSelector: '.gallery__slider',
   paginationSelector: '.gallery__pagination',
   nextButtonSelector: '.gallery__button-next',
   prevButtonSelector: '.gallery__button-prev',
 });
+
 observeReviewsSwiper('.reviews', {
   containerSelector: '.reviews__slider',
 });
