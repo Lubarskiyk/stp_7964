50540187467e56404fa938f9fa95aa05f9267ab2
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2025-02-23 15:18:31 +0200

    fix animation

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,4 +1,11 @@
 import AOS from 'aos';
 export function animate() {
-  AOS.init();
+  AOS.init({
+    once: false,
+    mirror: false,
+    easing: 'ease-out-cubic',
+    offset: 100,
+    duration: 500,
+    anchorPlacement: 'top-bottom',
+  });
 }
